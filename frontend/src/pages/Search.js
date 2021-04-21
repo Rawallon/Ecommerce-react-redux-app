f179dc5771f85fbe976d24b087364cf5e5125ad4
rawallon <rawallon@gmail.com>
2021-04-21 00:12:30 -0300

    Un-boostraped search page

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,12 +1,48 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Col, Row } from 'react-bootstrap';
-import Product from '../stories/components/CategoryProduct/';
+import styled from 'styled-components';
+
 import { listProducts } from '../actions/productActions';
+import Product from '../stories/components/CategoryProduct/';
 import PageSelect from '../components/PageSelect';
 import Meta from '../components/Meta';
 import Prefetch from '../components/Prefetch';
 import Breadcrumb from '../components/Breadcrumb/Breadcrumb';
+
+const Row = styled.div`
+  margin-top: 1.5rem;
+  margin-bottom: 1.5rem;
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+`;
+const Col = styled.div`
+  position: relative;
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+
+  @media (min-width: 576px) {
+    flex: 0 0 100%;
+    max-width: 100%;
+  }
+
+  @media (min-width: 768px) {
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
+
+  @media (min-width: 992px) {
+    flex: 0 0 33.333333%;
+    max-width: 33.333333%;
+  }
+
+  @media (min-width: 1200px) {
+    flex: 0 0 25%;
+    max-width: 25%;
+  }
+`;
 
 export function Search({ productList, listProducts, match }) {
   const { error, loading, products } = productList;
