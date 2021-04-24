bbfbb99b8654a9fcef4dd01044cd1020f965dc13
rawallon <rawallon@gmail.com>
2021-04-24 16:02:04 -0300

    Removed modal styles from inside its component

--- frontend/src/stories/pages/ProductPage/Modal/Modal.style.js
+++ frontend/src/stories/pages/ProductPage/Modal/Modal.style.js
@@ -10,3 +10,34 @@ export const Wrapper = styled.div`
     grid-template-columns: 1fr 1fr;
   }
 `;
+
+export const ModalStyle = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+`;
+
+export const ModalContent = styled.div`
+  border: 3px solid #f3f5f9;
+  width: 70%;
+  background: #fff;
+  padding: 2rem;
+  border-radius: 0.4rem;
+`;
+export const ModalBody = styled.div`
+  background: rgba(0, 0, 0, 0.6);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  /* left: -300%; */
+  transition: all 0.3s ease-in-out;
+  z-index: 2;
+`;
--- frontend/src/stories/pages/ProductPage/Modal/index.js
+++ frontend/src/stories/pages/ProductPage/Modal/index.js
@@ -1,50 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { connect } from 'react-redux';
+import { useHistory } from 'react-router';
 
 import {
   clearProductDetails,
   listProductDetails,
 } from '../../../../actions/productActions';
 import { addToCart } from '../../../../actions/cartActions';
-import { Wrapper } from './Modal.style';
-import { Container } from '../../../GlobalStyle.style';
 import PictureDisplay from '../PictureDisplay/';
 import ProductData from '../ProductData/';
 import Prefetch from '../../../../components/Prefetch';
-import styled from 'styled-components';
-import { useHistory } from 'react-router';
 
-const ModalStyle = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-`;
-
-const ModalContent = styled.div`
-  border: 3px solid #f3f5f9;
-  width: 70%;
-  background: #fff;
-  padding: 2rem;
-  border-radius: 0.4rem;
-`;
-const ModalBody = styled.div`
-  background: rgba(0, 0, 0, 0.6);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  width: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  /* left: -300%; */
-  transition: all 0.3s ease-in-out;
-  z-index: 2;
-`;
+import { Wrapper, ModalStyle, ModalContent, ModalBody } from './Modal.style';
+import { Container } from '../../../GlobalStyle.style';
 
 export function Modal({ productDetails, clearProductDetails, addToCart }) {
   const { error, loading, product, isModalOn } = productDetails || {
