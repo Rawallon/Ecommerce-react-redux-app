2fc8c884b67e19b224bbb463faafdcd715a43190
rawallon <rawallon@gmail.com>
2021-05-05 00:53:59 -0300

    Fixed directories on server.js

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,10 @@ const __dirname = path.resolve(); // Not avaliable because its using ESM
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'),
+      path.resolve(__dirname, 'frontend', 'build', 'index.html'),
     ),
   );
 } else {
